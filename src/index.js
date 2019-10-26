208183ccf87537863f1a2c7baf0c4928e3b607f0
Madi Peper <madisonpeper@gmail.com>
2019-10-26 11:13:22 -0500

    scaled character; added movement to character;

--- src/index.js
+++ src/index.js
@@ -27,6 +27,7 @@ const game = new Phaser.Game(config);
 let platforms;
 let player;
 let background;
+let cursors;
 
 function preload() {
   this.load.image('sky', sky);
@@ -48,14 +49,53 @@ function create() {
   platforms.create(400, 780, 'ground').setScale(4).refreshBody();
 
   // adding player to game
-  player = this.physics.add.sprite(100, 450, 'girl');
+  player = this.physics.add.sprite(600, 640, 'girl');
 
+  player.setScale(3);
   player.setBounce(0.2);
   player.setCollideWorldBounds(true);
 
+  // left animation
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('girl', { start: 0, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  // turn animation
+  this.anims.create({
+    key: 'turn',
+    frames: [{ key: 'girl', frame: 9 }],
+    frameRate: 20
+  });
+
+  // right animation
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('girl', { start: 10, end: 18 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
   this.physics.add.collider(player, platforms);
 
+  cursors = this.input.keyboard.createCursorKeys();
 }
 function update() {
+  if (cursors.left.isDown) {
+    player.setVelocityX(-250);
 
+    player.anims.play('left', true);
+  }
+  else if (cursors.right.isDown) {
+    player.setVelocityX(250);
+
+    player.anims.play('right', true);
+  }
+  else {
+    player.setVelocityX(0);
+
+    player.anims.play('turn');
+  }
 }
